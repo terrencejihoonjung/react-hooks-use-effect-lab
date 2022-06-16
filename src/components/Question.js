349651cc8192d3f62d02eab2b68170dd65a5e920
--replace-all <terrencejung928@gmail.com>
2022-06-16 01:48:07 -0700

    done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      if (timeRemaining === 0) {
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+      setTimeRemaining(timeRemaining => timeRemaining - 1)
+    }, 1000)
+
+    return function cleanup() {
+      clearTimeout(timeOutId);
+    }
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
